30c70dfcb901e40e12f153184e9ad4eb2db10db0
990Huzaifa <106827131+990Huzaifa@users.noreply.github.com>
2026-02-04 19:29:03 +0500

    model updated

--- app/Models/SentBox.php
+++ app/Models/SentBox.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class SentBox extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'from_email',
+        'to_email',
+        'subject',
+        'body_html',
+        'alias_id',
+        'mail_size',
+    ];
 }
--- app/Models/SentBoxAttachment.php
+++ app/Models/SentBoxAttachment.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class SentBoxAttachment extends Model
 {
     use HasFactory;
+
+
+    protected $fillable = [
+        'sent_mail_id',
+        'file_name',
+        'file_path',
+        'file_type',
+        'file_size',
+    ];
 }
